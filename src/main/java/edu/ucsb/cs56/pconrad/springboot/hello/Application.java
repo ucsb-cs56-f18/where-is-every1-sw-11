231a31ada304b4e9e8eb612d945b45771417c375
diane <dianephan12@gmail.com>
2018-11-20 10:50:34 -0800

    DP tried out database

--- src/main/java/edu/ucsb/cs56/pconrad/springboot/hello/Application.java
+++ src/main/java/edu/ucsb/cs56/pconrad/springboot/hello/Application.java
@@ -2,10 +2,34 @@ package edu.ucsb.cs56.pconrad.springboot.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-	SpringApplication.run(Application.class, args);
+        ArrayList<String> dbText = initDatabase();
+        final String displayText = makeString(dbText);
+        SpringApplication.run(Application.class, args);
     }
+
+    static ArrayList<String> initDatabase() {
+        ArrayList<String> dbQuery = new ArrayList<>();
+        try {
+            dbQuery = Database.createDocument();
+        } catch (UnknownHostException e) {
+            System.out.println("Unknown Host thrown");
+        }
+    return dbQuery;
+    }
+
+    static String makeString(ArrayList<String> text) {
+        String resultString = "";
+        for (String s: text) {
+            resultString += "<b> " + s + "</b><br/>";
+        }
+    return resultString;
+    }
+
 }
